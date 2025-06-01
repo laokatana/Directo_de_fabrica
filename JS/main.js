25404572a32fb09db634ec1e887710603aa64f6c
Lao <lao.tech.software@gmail.com>
2025-06-01 20:35:53 -0300

    proyecto inicializado

---
+++ JS/main.js
@@ -0,0 +1,44 @@
+async function loadComponent(containerId, path, cssPath, jsPath) {
+  if (cssPath) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = cssPath;
+
+    await new Promise((resolve) => {
+      link.onload = resolve;
+      document.head.appendChild(link);
+    });
+  }
+  
+
+  const html = await fetch(path).then((res) => res.text());
+  document.getElementById(containerId).innerHTML = html;
+
+  if (jsPath) {
+    const script = document.createElement("script");
+    script.src = jsPath;
+    script.defer = true;
+    document.body.appendChild(script);
+  }
+}
+
+
+loadComponent(
+  "navbar-container",
+  "./components/navbar/navbar.html",
+  "./components/navbar/navbar.css",
+  "./components/navbar/navbar.js"
+);
+
+loadComponent(
+  "home-container",
+  "./components/home/home.html",
+  "./components/home/home.css"
+);
+
+loadComponent(
+  "products-container",
+  "./components/products/products.html",
+  "./components/products/products.css",
+  "./components/products/products.js"
+);
---
+++ components/navbar/navbar.js
@@ -0,0 +1,14 @@
+// navbar.js (sin DOMContentLoaded)
+const toggleButton = document.querySelector(".navbar-toggle");
+const navLinks = document.querySelector(".navbar-links");
+const navItems = document.querySelectorAll("#nav-links a");
+
+document.getElementById("menu-toggle").addEventListener("click", () => {
+  document.getElementById("nav-links").classList.toggle("open");
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    document.getElementById("nav-links").classList.remove("open");
+  });
+});
